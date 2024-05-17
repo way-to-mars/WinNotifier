35475003a96ebb646503b6c9cdfdcbfa3286b5c7
way2mars <way2mars@hotmail.com>
2024-05-17 15:23:53 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+
+namespace WinNotifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebClient("...");
+            bool flag = false;
+            int counter = 10;
+
+            do
+            {
+                flag = client.Post(title: "C#", message: "OK");
+                counter--;
+            } while (!flag && counter > 0);
+
+            Console.WriteLine(flag);
+
+        }
+    }
+}
---
+++ WebClient.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+
+namespace WinNotifier
+{
+    internal class WebClient(string apiKey, int timeOutSeconds = 10)
+    {
+        private string apiKey = apiKey;
+        private int timeOutSeconds = timeOutSeconds;
+
+        private HttpClient? ProvideHttpClient() {
+            // определяем коллекцию сервисов
+            var services = new ServiceCollection();
+            // добавляем сервисы, связанные с HttpClient, в том числе IHttpClientFactory
+            services.AddHttpClient();
+            // создаем провайдер сервисов
+            var serviceProvider = services.BuildServiceProvider();
+            // получаем сервис IHttpClientFactory
+            var httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+            // создаем объект HttpClient
+            return httpClientFactory?.CreateClient();
+        }
+
+        public bool Post(string title, string message) {
+
+            HttpClient? httpClient = ProvideHttpClient();
+            if (httpClient == null) return false;
+            try
+            {
+                Dictionary<string, string> values = new()
+                {
+                    { "ApiKey", apiKey },
+                    { "PushTitle", title },
+                    { "PushText", message }
+                };
+
+                HttpContent content = new FormUrlEncodedContent(values);
+
+                var response = httpClient.PostAsync("https://www.notifymydevice.com/push", content).Result;
+
+                var responseString = response.Content.ReadAsStringAsync().Result;
+
+                Console.WriteLine(responseString);
+
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+        }
+    }
+}
